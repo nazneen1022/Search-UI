a063cc2eab54f29fd71c1fb9eba110e10fae370b
nazneen1022 <nazneen1022@gmail.com>
2020-11-21 23:42:36 +0100

    removed console log statements

--- src/store/Products/actions.js
+++ src/store/Products/actions.js
@@ -1,4 +1,3 @@
 export const setProducts = (products) => {
-  console.log("in action:", products);
   return { type: "GET_SEARCH_PRODUCTS", payload: products };
 };
--- src/store/Products/reducer.js
+++ src/store/Products/reducer.js
@@ -1,7 +1,6 @@
 const initialState = null;
 
 export default function reducer(state = initialState, action) {
-  console.log("I'm here:", action);
   switch (action.type) {
     case "GET_SEARCH_PRODUCTS": {
       return action.payload;
