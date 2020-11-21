dcc2daecb30d6f58634b4216baf904c15f8d5212
nazneen1022 <nazneen1022@gmail.com>
2020-11-21 21:03:13 +0100

    dispatch setProducts action

--- src/Components/SearchBar/index.tsx
+++ src/Components/SearchBar/index.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getData } from "../../store/Source/selectors";
+import { setProducts } from "../../store/Products/actions";
 import "../../App.css";
 
 export default function SearchBar() {
+  const dispatch = useDispatch();
+  const data = useSelector(getData);
+
+  useEffect(() => {
+    data.content && dispatch(setProducts(data.content.products));
+  }, [data, dispatch]);
+
   return (
     <div>
       <div className="search-container">
